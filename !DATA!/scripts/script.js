d565e57b462562d0722f2e44ddd7bd19e1d70d95
literallybenjam <bjmns@me.com>
2015-03-12 01:40:15 -0700

    Initial javascripting: div loading, span selecting

--- !DATA!/scripts/script.js
+++ !DATA!/scripts/script.js
@@ -1,1 +1,60 @@
+/* jslint asi:true, browser:true */
 
+var stage = 0;
+var current_section = null;
+var current_div = null;
+
+function advanceDiv() {
+
+    unloadDiv();
+    current_div = current_div.nextElementSibling;
+    if (current_div) loadDiv();
+    else {
+
+        switch (stage) {
+
+            case 0:
+                loadSection("egg-clusive");
+                break;
+
+            default:
+                break;
+
+        }
+        stage++;
+
+    }
+
+}
+
+function selectOption() {
+    this.dataset.selected = "";
+    advanceDiv();
+}
+
+function unloadDiv() {
+    var i;
+    current_div.dataset.mode = "1";
+    for (i = 0; i < current_div.getElementsByTagName("span"); i++) {
+        current_div.getElementsByTagName("span").item(i).removeEventListener("click", selectOption, false);
+    }
+}
+
+function loadDiv() {
+    var i;
+    current_div.dataset.mode = "0";
+    for (i = 0; i < current_div.getElementsByTagName("span"); i++) {
+        current_div.getElementsByTagName("span").item(i).addEventListener("click", selectOption, false);
+    }
+}
+
+function loadSection(id) {
+    current_section = document.getElementById(id);
+    current_div = current_section.children.item(0);
+}
+
+function init() {
+    loadSection("egg-size");
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
